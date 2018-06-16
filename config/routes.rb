8b7c77d0101fc268a412393fa2b8fc33e898736b
ronen.goren <ronen.goren@gmail.com>
2018-06-16 01:23:56 -0400

    test ronen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  
+  root 'calendars#index'
+
   resources :calendar
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
