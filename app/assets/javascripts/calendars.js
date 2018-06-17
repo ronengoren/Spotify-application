1535a940ee9b2cb63ebc007c99181b6b8ed695c5
ronen.goren <ronen.goren@gmail.com>
2018-06-17 00:37:31 -0400

    modal work

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -71,19 +71,26 @@ function displayCalendar() {
         // if counter is current day.
         // highlight current day using the CSS defined in header.
         if (counter == day) {
-            htmlContent += "<li class='days' onclick='alert(\"work!\")' onMouseOver='this.style.background=\"#FF0000\"; this.style.color=\"#FFFFFF\"' " +
-                "onMouseOut='this.style.background=\"#FFFFFF\"; this.style.color=\"#00FF00\"'>" + nextMonth + "/" + counter + "/" + year + "</li>";
+            htmlContent += "<li class='days' onMouseOver='this.style.background=\"#FF0000\"; this.style.color=\"#FFFFFF\"' " +
+                "onMouseOut='this.style.background=\"#FFFFFF\"; this.style.color=\"#00FF00\"'><button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Open Modal</button>" + nextMonth + "/" + counter + "/" + year + "</li>";
+
         } else {
-            htmlContent += "<li class='days' onclick='alert(\"work!\")' onMouseOver='this.style.background=\"#FF0000\"'" +
-                " onMouseOut='this.style.background=\"#FFFFFF\"'>" + nextMonth + "/" + counter + "/" + year + "</li>";
+            htmlContent += "<li class='days' onMouseOver='this.style.background=\"#FF0000\"'" +
+                " onMouseOut='this.style.background=\"#FFFFFF\"'><button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Open Modal</button>" + nextMonth + "/" + counter + "/" + year + "</li>";
+
 
         }
 
         weekdays2++;
         counter++;
+
     }
 
+    function modalPopup() {
+        console.log('clickCount')
 
+
+    }
 
     // building the calendar html body.
     var calendarBody = " <ul class='calendar'><li class='month'><div colspan='7'>" +
@@ -96,59 +103,8 @@ function displayCalendar() {
     // set the content of div .
     document.getElementById("calendargrid").innerHTML = calendarBody;
 
-    function modalPopup() {
-        $('td').on('click', function(e) {
-            $('#myModal').on('show.bs.modal', function(e) {
-                if (!data)
-                    return e.preventDefault() // stops modal from being shown
-            })
-        })
-    }
 
-    // var clickCount = 0
-    // var clickCountMax = 2;
 
-    // function clickEvent() {
-    // $('.monthNow').on('click', function(e) {
-    // getDescription()
-    // e.preventDefault();
-    // $(this).find('.card').toggleClass('flipped');
-    // click.push($(this).find('img').attr('src'))
-    // clickCount++
-    // console.log(clickCount)
-    // if (clickCount == 2) {
-    //     $('.flip').off('click');
-    //     e.preventDefault();
-    //     check();
-    //     // game over; code to handle that
-    // }
 
-    // });
 
 }
-
-// }
-
-// function getDescription() {
-//     //wrap in prompt box
-//     eventDescription = prompt("ENTER A DESCRIPTION OF YOUR EVENT:");
-
-//     if (eventDescription != null) {
-//         //get date from context click 
-//         date = $(this).getAttribute("id");
-//         date = +date;
-//         console.log(date); //testing
-//         //write the description to the DOM
-//         console.log(eventDescription); //testing
-//         (function() { //pass in date as variable
-//             document.getElementById(date).innerHTML = date + "&emsp;" + eventDescription;
-//             //add class when selected
-//             document.getElementById(date).className = "calendarCellSelected";
-//         })(); //this will always execute when parent function called
-//     };
-// }
-
-// function displayDescription(entryId) {
-//     //writes description to correct ID in DOM
-//     //refactored as IIFE in getDescription() above
-// }
