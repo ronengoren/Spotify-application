3099c7eb63bb924466eb8a1415844b4685b911c6
ronen.goren <ronen.goren@gmail.com>
2018-06-17 00:59:35 -0400

    added events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_042613) do
+ActiveRecord::Schema.define(version: 2018_06_17_044215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2018_06_16_042613) do
   create_table "calendars", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
   end
 
 end
