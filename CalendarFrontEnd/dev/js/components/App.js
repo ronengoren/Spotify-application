929c4e73420c9246ff63944566675bd0ba79f7ef
ronen.goren <ronen.goren@gmail.com>
2018-06-23 14:22:07 -0400

    added react calendar

--- CalendarFrontEnd/dev/js/components/App.js
+++ CalendarFrontEnd/dev/js/components/App.js
@@ -1,15 +1,42 @@
-import React, {Component} from 'react';
+import React from "react";
+import Calendar from "./Calendar";
 import "../../scss/style.scss";
-class App extends Component{
-  render(){
-    return(
-      <div> 
-      Navbar
-      {this.props.children}
-      </div> 
-    )
 
+
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div> 
+//       Navbar
+//       {this.props.children}
+//       </div> 
+//     )
+
+//   }
+// }
+
+// import "App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
   }
 }
 
 export default App;
+
